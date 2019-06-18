9a73431fb13724f1ac5673f01ccfe83ad631ddeb
Laura Sheach <laura_bubble_git@yahoo.com>
2019-06-18 21:13:33 +0100

    MVP completed

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      currencies: {}
+      currencies: {},
+      euroAmount: 0,
+      selectedCurrency: 0,
+      currencyAmount: 0,
+      selectedCurrencyToEuro: 0
     },
     mounted(){
       this.getCurrencies()
@@ -15,6 +19,16 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(result => result.json())
         .then(currencies => this.currencies = currencies)
       }
+    },
+    computed: {
+      euroToCurrency: function(){
+          const result = this.euroAmount * this.selectedCurrency;
+          return result.toFixed(2);
+        },
+        currencyToEuro: function(){
+          const currency = this.currencyAmount / this.selectedCurrencyToEuro;
+          return currency.toFixed(2);
+        }
     }
   });
 });
